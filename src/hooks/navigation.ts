1fa9d3104360e11d888cc58bc9eee910597d2853
Oscar Alarcon <oalarcon.contractor@luminatedata.com>
2024-04-25 20:51:38 -0600

    Fix import

--- src/hooks/navigation.ts
+++ src/hooks/navigation.ts
@@ -1,5 +1,5 @@
 import {useNavigation} from '@react-navigation/native';
-
+import {StackNavigation} from '../../app/navigation/navigation';
 
 export const useNavigate = () => {
   const navigation = useNavigation<StackNavigation>();
